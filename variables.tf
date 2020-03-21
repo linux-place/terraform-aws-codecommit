63e9b3e6eb1cae9c3e10f899c93667068f3444e3
Fernando Augusto Medeiros Silva <fernando.augusto@gmail.com>
2020-03-21 10:35:59 -0300

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+  
+variable "repository_name" {
+  type        = string
+  description = "The name of your GIT repository"
+}
+
+variable "default_branch" {
+  type        = string
+  description = "The name of the default repository branch"
+  default     = "master"
+}
+
